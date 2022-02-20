964b13a3976bf495f4136b5e42873fa54b3ddcbf
fanxping <937212680@qq.com>
2022-02-20 11:32:06 +0800

    add switch language

--- app/src/components/PageFooter.vue
+++ app/src/components/PageFooter.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="w-full flex flex-row justify-between items-center box-border footer">
-    <img src="/images/logo_footer.png" width="190">
-    <div class="text-light-9a text-size-16">Copyright © GameYoo 2022. All rights reserved.</div>
+  <div
+    class="w-full flex flex-row justify-between items-center box-border footer">
+    <div class="flex items-center">
+      <img src="/images/logo_footer.png" width="190" />
+      <switch-language class="ml-space-32"></switch-language>
+    </div>
+    <div class="text-light-9a text-size-16">
+      Copyright © GameYoo 2022. All rights reserved.
+    </div>
   </div>
 </template>
+<script setup>
+import SwitchLanguage from './SwitchLanguage.vue'
+</script>
 <style lang="scss" scoped>
-.footer{
+.footer {
   padding: 1.2rem 0;
   max-width: var(--page-max-width);
 }
---
+++ app/src/components/SwitchLanguage.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-select v-model="language" effect="dark">
+    <el-option
+      v-for="(item, index) in LANGUAGES"
+      :key="index"
+      :label="item.text"
+      :value="item.type"></el-option>
+  </el-select>
+</template>
+<script setup>
+import { ref, watch } from 'vue'
+import { useStore } from 'vuex'
+import { LANGUAGE_EN_US, LANGUAGE_ZH_HK } from '@/store/mutation-types'
+
+const store = useStore()
+
+const ACTION_EN = { text: 'English', type: LANGUAGE_EN_US }
+const ACTION_HK = { text: '繁體中文', type: LANGUAGE_ZH_HK }
+
+const LANGUAGES = [ACTION_HK, ACTION_EN]
+
+const language = ref(store.getters.lang)
+watch(language, (v) => {
+  store.dispatch('setLang', v)
+})
+</script>
+<style lang="scss" scoped>
+:deep(.el-input) {
+  background: transparent;
+}
+:deep(.el-input__inner) {
+  background: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  border-radius: 0.6rem;
+  height: 2rem;
+  color: var(--light-9a);
+  text-align: start;
+}
+</style>
